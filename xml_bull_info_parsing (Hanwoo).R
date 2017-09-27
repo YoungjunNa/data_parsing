df80e5746b3777640626938a7eaa4c7816c9bd9c
Youngjun Na <ruminoreticulum@gmail.com>
2017-09-27 10:45:34 +0900

    Update xml_bull_info_parsing (Hanwoo).R

--- xml_bull_info_parsing (Hanwoo).R
+++ xml_bull_info_parsing (Hanwoo).R
@@ -5,19 +5,20 @@
 #Github: https://github.com/YoungjunNa
 #Date: 9/27/2017
 
-#Reference: http://hanwoori.nias.go.kr/openapi/openApiIndex.jsp
-
 bull_info<-function(KPN){
   #package
   require("XML")
   
-    #import basic informations
+  #import bull informations
   url1<-paste("http://hanwoori.nias.go.kr/openapi/brblInfoOk.jsp?dataType=xml&brblNo=","KPN",KPN,sep="")
   xmlfile1<-xmlParse(url1)
   xmltop1<-xmlRoot(xmlfile1)
   get_inform<-xmlToDataFrame(getNodeSet(xmlfile1,"//item"),stringsAsFactors=FALSE)
   
-    print(get_inform)
+  get_inform<-get_inform[-c(1,2,3,10,11,12,13,14,20,22,23,24,25,26,29,31,38,39,40,41,43,45,53,54)]
+  
+  #print
+  print(get_inform)
 }
 
-bull_info(634)
+bull_info(1080)
